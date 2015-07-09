d474e762f85c85cbb1c3b4c44cf5099fba4ff6e1
Bala Clark <bala.clark@clock.co.uk>
2015-07-09 18:53:49 +0200

    improve tests

--- test/validator.test.js
+++ test/validator.test.js
@@ -14,8 +14,32 @@ describe('Validity Validate Each', function () {
     })
   })
 
-  it('should not return errors on empty arrays', function (done) {
+  it('should validate a list of invalid entries', function (done) {
+    var obj = { test: [ undefined, '', null ] }
+      , validateEach = createValidator(required)
+
+    validateEach('test', 'Test Property', obj, function (err, msgs) {
+      assert.deepEqual(msgs
+        , { 0: 'Test Property is required'
+          , 1: 'Test Property is required'
+          , 2: 'Test Property is required'
+          })
+      done()
+    })
+  })
+
+  it('should not return errors if the requested property does not exist', function (done) {
     var obj = {}
+      , validateEach = createValidator(required)
+
+    validateEach('test', 'Test Property', obj, function (err, msgs) {
+      assert(!msgs)
+      done()
+    })
+  })
+
+  it('should not return errors if the requested property is an empty array', function (done) {
+    var obj = { test: [] }
       , validateEach = createValidator(required)
 
     validateEach('test', 'Test Property', obj, function (err, msgs) {
