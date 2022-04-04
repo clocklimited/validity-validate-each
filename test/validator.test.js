3e51a43d8a22edd0cf17e92a82485e6a79be3644
Wes Moore <rkidwes@hotmail.com>
2022-04-04 16:35:19 +0100

    chore: Switch to Clock Scoped Validity

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,50 +1,52 @@
-var assert = require('assert-diff')
-  , required = require('validity').required
-  , createValidator = require('..')
+var assert = require("assert-diff"),
+  required = require("@clocklimited/validity").required,
+  createValidator = require("..");
 
-describe('Validity Validate Each', function () {
+describe("Validity Validate Each", function () {
+  it("should apply a given validator to each value of an array", function (done) {
+    var obj = { test: [1, 2, "", null, 5] },
+      validateEach = createValidator(required);
 
-  it('should apply a given validator to each value of an array', function (done) {
-    var obj = { test: [ 1, 2, '', null, 5 ] }
-      , validateEach = createValidator(required)
+    validateEach("test", "Test Property", obj, function (err, msgs) {
+      assert.deepEqual(msgs, {
+        2: "Test Property is required",
+        3: "Test Property is required",
+      });
+      done();
+    });
+  });
 
-    validateEach('test', 'Test Property', obj, function (err, msgs) {
-      assert.deepEqual(msgs, { 2: 'Test Property is required', 3: 'Test Property is required' })
-      done()
-    })
-  })
+  it("should validate a list of invalid entries", function (done) {
+    var obj = { test: [undefined, "", null] },
+      validateEach = createValidator(required);
 
-  it('should validate a list of invalid entries', function (done) {
-    var obj = { test: [ undefined, '', null ] }
-      , validateEach = createValidator(required)
+    validateEach("test", "Test Property", obj, function (err, msgs) {
+      assert.deepEqual(msgs, {
+        0: "Test Property is required",
+        1: "Test Property is required",
+        2: "Test Property is required",
+      });
+      done();
+    });
+  });
 
-    validateEach('test', 'Test Property', obj, function (err, msgs) {
-      assert.deepEqual(msgs
-        , { 0: 'Test Property is required'
-          , 1: 'Test Property is required'
-          , 2: 'Test Property is required'
-          })
-      done()
-    })
-  })
+  it("should not return errors if the requested property does not exist", function (done) {
+    var obj = {},
+      validateEach = createValidator(required);
 
-  it('should not return errors if the requested property does not exist', function (done) {
-    var obj = {}
-      , validateEach = createValidator(required)
+    validateEach("test", "Test Property", obj, function (err, msgs) {
+      assert(!msgs);
+      done();
+    });
+  });
 
-    validateEach('test', 'Test Property', obj, function (err, msgs) {
-      assert(!msgs)
-      done()
-    })
-  })
+  it("should not return errors if the requested property is an empty array", function (done) {
+    var obj = { test: [] },
+      validateEach = createValidator(required);
 
-  it('should not return errors if the requested property is an empty array', function (done) {
-    var obj = { test: [] }
-      , validateEach = createValidator(required)
-
-    validateEach('test', 'Test Property', obj, function (err, msgs) {
-      assert(!msgs)
-      done()
-    })
-  })
-})
+    validateEach("test", "Test Property", obj, function (err, msgs) {
+      assert(!msgs);
+      done();
+    });
+  });
+});
